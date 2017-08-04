161c10f62bd9f58dde2c2a445ddbcdb8e2c9c418
hong-wei <zhw_gz@126.com>
2017-08-04 12:27:27 +0200

    works well

--- src/main/scala/LearnJson/BK2.scala
+++ src/main/scala/LearnJson/BK2.scala
@@ -1,32 +1,6 @@
 package LearnJson
 
 
-/**
-  *
-  */
-//BK 1 when ,how to use None, isEmpty ???who has these feature???
-
-//BK 2 P168 ,why use tree structure to store data??
-
-//BK day01 Swagger
-//  /Users/zhanghongwei/Documents/workspaceScala/LoveScala/src/main/scala/Swagger
-
-
-
-class BK2 {}
-//BK 1 learn sealed 关键字
- //check 14.14 Sealed Classes in Chapter14.scala
-
-//BK 2 Learn Lift Json
-
-
-//BK 3 Learn Future
-
-
-import argonaut.Argonaut._
-import argonaut._
-
-
 sealed trait TransactionRequestCommonBodyJSON {
   val value : AmountOfMoneyJsonV121
   val description: String
@@ -69,14 +43,14 @@ case class CreateTransaction(
 
 object a1 extends App{
 
-  val bodyValue = AmountOfMoneyJsonV121("EUR", "1000")
-
-  val transactionRequestBodyCounterpartyJSON = TransactionRequestBodyCounterpartyJSON(
-    bodyValue,
-    "A description for the transaction to the counterparty",
-    "SHARED", 
-    "hongwei"
-  )
+//  val bodyValue = AmountOfMoneyJsonV121("EUR", "1000")
+//
+//  val transactionRequestBodyCounterpartyJSON = TransactionRequestBodyCounterpartyJSON(
+//    bodyValue,
+//    "A description for the transaction to the counterparty",
+//    "SHARED", 
+//    "hongwei"
+//  )
   // encoding
 //  val encode = EncodeJson.of[CC]
 //
@@ -100,24 +74,24 @@ object a1 extends App{
 //  val result = decode.decodeJson(json)
 
 
-  val aa = CreateTransaction(
-    // fromAccount
-    fromAccountId = "8ca8a7e4-6d02-48e3-a029-0b2bf89de9f0",
-    fromAccountBankId = "gh.29.uk",
-
-    // transaction details
-    transactionRequestType = "SANDBOX_TAN",
-    transactionChargePolicy = "RECEIVER",
-    transactionRequestCommonBody= transactionRequestBodyCounterpartyJSON,
-    // toAccount or toCounterparty
-    toCounterpartyId = "1234",
-    toCounterpartyName = "obp",
-    toCounterpartyCurrency = "EUR",
-    toCounterpartyRoutingAddress = "1234",
-    toCounterpartyRoutingScheme = "OBP",
-    toCounterpartyBankRoutingAddress = "12345",
-    toCounterpartyBankRoutingScheme = "OBP"
-  )
+//  val aa = CreateTransaction(
+//    // fromAccount
+//    fromAccountId = "8ca8a7e4-6d02-48e3-a029-0b2bf89de9f0",
+//    fromAccountBankId = "gh.29.uk",
+//
+//    // transaction details
+//    transactionRequestType = "SANDBOX_TAN",
+//    transactionChargePolicy = "RECEIVER",
+//    transactionRequestCommonBody= transactionRequestBodyCounterpartyJSON,
+//    // toAccount or toCounterparty
+//    toCounterpartyId = "1234",
+//    toCounterpartyName = "obp",
+//    toCounterpartyCurrency = "EUR",
+//    toCounterpartyRoutingAddress = "1234",
+//    toCounterpartyRoutingScheme = "OBP",
+//    toCounterpartyBankRoutingAddress = "12345",
+//    toCounterpartyBankRoutingScheme = "OBP"
+//  )
 //  implicit val formats = net.liftweb.json.DefaultFormats
 //  val decompose1 = decompose(aa)
 //  val compact1 = compact(render((decompose1)))
@@ -131,14 +105,47 @@ object a1 extends App{
 //  val json2: Json = encode2(aa)
 //  private val nospacesString = json2.nospaces
 //  println(nospacesString)
-  
-  
-  
-  private val x: CreateTransaction = """{"transactionRequestType":"SANDBOX_TAN","toCounterpartyRoutingAddress":"1234","toCounterpartyBankRoutingScheme":"OBP","fromAccountId":"8ca8a7e4-6d02-48e3-a029-0b2bf89de9f0","transactionChargePolicy":"RECEIVER","toCounterpartyId":"1234","toCounterpartyRoutingScheme":"OBP","toCounterpartyBankRoutingAddress":"12345","toCounterpartyName":"obp","toCounterpartyCurrency":"EUR","transactionRequestCommonBody":{"TransactionRequestBodyCounterpartyJSON":{"hongwei":"hongwei","charge_policy":"SHARED","description":"A description for the transaction to the counterparty","value":{"amount":"1000","currency":"EUR"}}},"fromAccountBankId":"gh.29.uk"}""".decodeOption[CreateTransaction].get
-  println(x)
 
 
-// decoding
+  import argonaut.{JsonIdentity=> _,_}
+  import Argonaut._
+  import ArgonautShapeless._
+  val x: CreateTransaction = """{"transactionRequestType":"SANDBOX_TAN","toCounterpartyRoutingAddress":"1234","toCounterpartyBankRoutingScheme":"OBP","fromAccountId":"8ca8a7e4-6d02-48e3-a029-0b2bf89de9f0","transactionChargePolicy":"RECEIVER","toCounterpartyId":"1234","toCounterpartyRoutingScheme":"OBP","toCounterpartyBankRoutingAddress":"12345","toCounterpartyName":"obp","toCounterpartyCurrency":"EUR","transactionRequestCommonBody":{"TransactionRequestBodyCounterpartyJSON":{"hongwei":"hongwei","charge_policy":"SHARED","description":"A description for the transaction to the counterparty","value":{"amount":"1000","currency":"EUR"}}},"fromAccountBankId":"gh.29.uk"}""".decodeOption[CreateTransaction].get
+//  private val x2: CreateTransaction = """{
+//                                          "fromAccountBankId": "10",
+//                                          "fromAccountId": "tKWSUBy6sha3Vhxc_vw9OK96a0RprtoxUuObMYR29TI",
+//                                          "transactionRequestType": "PHONE_TO_PHONE",
+//                                          "transactionChargePolicy": "SHARED",
+//                                          "transactionRequestCommonBody": {
+//                                            "from_account_phone_number": "+9722398746",
+//                                            "value": {
+//                                              "currency": "ILS",
+//                                              "amount": "1"
+//                                            },
+//                                            "description": "ABC",
+//                                            "charge_policy": "SHARED",
+//                                            "couterparty": {
+//                                              "other_account_owner": "TOM",
+//                                              "other_account_owner_birthday": "19900810",
+//                                              "other_account_phone_number": "+9722398746"
+//                                            }
+//                                          },
+//                                          "toCounterpartyId": "",
+//                                          "toCounterpartyName": "",
+//                                          "toCounterpartyCurrency": "ILS",
+//                                          "toCounterpartyRoutingAddress": "",
+//                                          "toCounterpartyRoutingScheme": "",
+//                                          "toCounterpartyBankRoutingAddress": "",
+//                                          "toCounterpartyBankRoutingScheme": ""
+//                                        }""".decodeOption[CreateTransaction].get
+  println(x.asJson)
+
+
+
+//  println(x.asJons.nospaces)
+
+  
+  // decoding
 //  val decode2 = DecodeJson.of[CreateTransaction]
 //
 //  val result2 = decode2.decodeJson(json2)
