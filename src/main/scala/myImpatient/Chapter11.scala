78f26a84bf63a8332dd887dcc1d2b8336ac4f7e5
hong-wei <zhw_gz@126.com>
2017-11-05 12:45:21 +0100

    read chapter 11 Operators

--- src/main/scala/myImpatient/Chapter11.scala
+++ src/main/scala/myImpatient/Chapter11.scala
@@ -4,7 +4,7 @@ package myImpatient
   * Created by zhanghongwei on 16/11/16.
   */
 object Identifiers extends App{
-  //BK 11.1 Identifiers — page 131 •
+  //BK 11.1 Identifiers
   //scala support more characters.
   // all the three are not support
   val `()[]{}` = 8
@@ -23,50 +23,70 @@ object Identifiers extends App{
 }
 
 
-//BK 11.2 Infix Operators — page 132 •
+//BK 11.2 Infix Operators
+//Scala的操作符不是特殊的语言语法, 任何方法都可以是操作符
+//操作符分为前缀, 中缀, 后缀
+//Scala中所有操作符都是方法调用
+//你可以把任何方法都当作操作符来标注
+//任何方法都可以是操作符
+//Scala里的操作符不是特殊的语言语法：任何方法都可以是操作符。使用方法的方式使它成为操作符。
+//如果写成s.indexOf('o')，indexOf就不是操作符。不过如果写成，s indexOf 'o'，
+//那么indexOf就是操作符了，因为你以操作符标注方式使用它
 object InfixOperators2 extends App {
+  // infix mean, the operator is between the arguments
   // a identifier b where 
   // identifier denotes a method with two parameters (one implicit, one explicit). 
   // For example, the expression
   1 to 10
   //actuallt this 
-
-  import scala.collection.immutable.Range.Inclusive
-  import scala.math._
-
+  1.to(10) 
+  // def to(end: Int): Range.Inclusive = Range.inclusive(self, end), 
+  // self is the implicit one and end is the explicit one .
+  "x.toLowerCase".toLowerCase
+  "x.toLowerCase" toLowerCase
   class Fraction(n: Int, d: Int) {
     private val num: Int = if (d == 0) 1 else n * sign(d) / gcd(n, d);
     private val den: Int = if (d == 0) 0 else d * sign(d) / gcd(n, d);
 
     override def toString = num + "/" + den
-
-    def sign(a: Int) = if (a > 0) 1 else if (a < 0) -1 else 0
-
-    def gcd(a: Int, b: Int): Int = if (b == 0) abs(a) else gcd(b, a % b)
-
+    //You can design your own fix identifier, as the scala build in ones
     def *(other: Fraction) = new Fraction(num * other.num, den * other.den)
+    def sign(a: Int) = if (a > 0) 1 else if (a < 0) -1 else 0
+    def gcd(a: Int, b: Int): Int = if (b == 0) scala.math.abs(a) else gcd(b, a % b)
   }
 
   val f1 = new Fraction(3, 4)
   val f2 = new Fraction(2, 5)
   println(f1 * f2)
 
-  private val tuple: (Int, Int) = 1 -> 10
-  private val inclusive: Inclusive = 1 to 10
-  private val to: Inclusive = 1.to(10)
-
 }
 
-//BK 11.3 Unary Operators — page 133 •
+//BK 11.3 Unary Operators
+//http://developer.51cto.com/art/200907/137999.htm
+//本文节选自Martin Odersky，Lex Spoon和Bill Venners所著，Regular翻译的《Programming in Scala》的第五章。
+//Scala是一种针对 JVM 将函数和面向对象技术组合在一起的编程语言
+//Scala的操作符：任何方法都可以是操作符
 object UnaryOperators3 extends App {
-  //eg1 : postfix
-  1 toString
-
+  // Infix operators are binary operators—they have two parameters. 
+  // An operator with one parameter is called a unary operator. 
+  // If it follows the argument, it is a postfix operator. The expression
+  // the Operator is following the parameter
+  //BK 11.3.1 postfix
+//  后缀操作符是不用点或括号调用的不带任何参数的方法
+  1 toString()// ??? there need some endpoint. 
+    // equals the following
   1.toString()
+    // equals the following
+   "" toLowerCase
 
+  //BK 11.3.2 perfix
+  //可以当作前缀操作符用的标识符只有+，-，!和~
+  //4 operators +, -, !, ~ are allowed as prefix operators, appearing before their arguments.
+  //They are converted into calls to methods with the name unary_operator.
   //eg2:prefix +,-,!,~
   val a = 42
   -a
+  //The same as a.unary_-.
   a.unary_-
 
 }
@@ -96,7 +116,7 @@ object Precedence5 extends App {
   1 + 2 to 10 // to has lower precedence than +
   1 -> 2 * 3 // * has higher precedence than ->
   1 + 2 -> 3 // + has the same precedence as ->
-  //  1 -> 2 + 3 // Error--can't apply + to a pair
+  //  1 -> 2 + 3 // Error--can't` apply + to a pair
   1 to 10 toString // Postfix operators have lower precedence than infix
 }
 
@@ -150,7 +170,6 @@ object ApplyUpapply7 extends App {
 //BK 11.8 Extractors
 //An extractor is an object with an unapply method
 object Extractors8 extends App {
-
   //eg1: opposite from apply
   //apply
   private val fraction: Fraction = Fraction(3, 4)
